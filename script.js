7c3ac91b0bf60aa3d4423ae1d825fc4c7ce06d6a
tango <sm91wilson@gmail.com>
2020-02-27 12:51:33 -0500

    added var for all the buttons and elements

---
+++ script.js
@@ -0,0 +1,15 @@
+var start = document.getElementById("start");
+var quiz = document.getElementById("question-box");
+var answer1 = document.getElementById("btn1");
+var answer2 = document.getElementById("btn2");
+var answer3 = document.getElementById("btn3");
+var answer4 = document.getElementById("btn4");
+var feedback = document.getElementById("feedback");
+
+// function to begin the quiz
+function beginQuiz(){}
+
+// function to show score\
+function showScore(){}
+
+// use an array of objects for the questions
